7963c7f87b13773f0eb7bb524cb9e1c2adb63606
ascodeasice <ab0988956087@gmail.com>
2022-07-27 11:34:13 +0800

    Add button animation.

--- shopping-cart/src/components/HomePage.js
+++ shopping-cart/src/components/HomePage.js
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div id='homePage' className='pageContent'>
       <h1 id='slogan'>"No, you pronounced it wrong!"</h1>
-      <Link id='shopBtnLink' to='/shop'><button id="shopBtn">Shop Now</button></Link>
+      <Link id='shopBtnLink' to='/shop'><button id="shopBtn" className="grow_ellipse">Shop Now</button></Link>
     </div>
   );
 };
