9521b3db739e3b211352b729d91aaf94d4bf4644
ascodeasice <ab0988956087@gmail.com>
2022-07-28 11:17:43 +0800

    Change the icon appearance. Make it more like IKEA.

--- shopping-cart/src/components/Navbar.js
+++ shopping-cart/src/components/Navbar.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div id='navBar'>
-      <p id='title'>EE-KAY-UH</p>
+      <div id="iconWrapper">
+        <p id='title'>EE-KAY-UH</p>
+      </div>
       <div id='navBarWrapper'>
         <Link className="navBarLink" to='/'>Home</Link>
         <Link className="navBarLink" to='/shop'>Shop</Link>
