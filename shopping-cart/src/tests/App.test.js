dddb27f7bbb9d4b02eaa283e1ccbb47712b3d439
ascodeasice <ab0988956087@gmail.com>
2022-07-28 11:45:30 +0800

    Finish testing NavBar component.

---
+++ shopping-cart/src/tests/App.test.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom'
+import App from '../App';
+
+describe('HomePage Component', () => {
+  it('render text and button', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>);
+    const heading = screen.getByRole('heading');
+    const button = screen.getByRole('button', { name: 'Shop Now' });
+
+    expect(heading.textContent).toBe('"No, you pronounced it wrong!"');
+    expect(button).toBeInTheDocument();
+  })
+  it('click Shop Now button go to shop page', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const button = screen.getByRole('button', { name: 'Shop Now' });
+    userEvent.click(button);
+
+    expect(screen.getByRole('heading').textContent).toBe('Shop Page');
+  })
+});
+
+describe('NavBar component', () => {
+  it('render icon and links', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const icon = screen.getByText('EE-KAY-UH');
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const shopLink = screen.getByRole('link', { name: 'Shop' });
+    const cartLink = screen.getByRole('link', { name: 'Cart' });
+
+    expect(icon).toBeInTheDocument();
+    expect(homeLink).toBeInTheDocument();
+    expect(shopLink).toBeInTheDocument();
+    expect(cartLink).toBeInTheDocument();
+  });
+
+  it('click links go to right pages', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const shopLink = screen.getByRole('link', { name: 'Shop' });
+    const cartLink = screen.getByRole('link', { name: 'Cart' });
+
+    userEvent.click(shopLink);
+    expect(screen.getByRole('heading').textContent).toBe('Shop Page');
+    userEvent.click(cartLink);
+    expect(screen.getByRole('heading').textContent).toBe('Cart Page');
+    userEvent.click(homeLink);
+    expect(screen.getByRole('heading').textContent).toBe('"No, you pronounced it wrong!"');
+
+  })
+});
